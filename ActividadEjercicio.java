f1a085c1c14c5ed48a54de5e04558ac33926b70e
Ian Cumes <ianrodrigocumes@gmail.com>
2023-11-22 22:24:22 -0600

    Entrega Final Proyecto Nutricion V2

--- ActividadEjercicio.java
+++
@@ -1,9 +0,0 @@
-//Clase dedicada a la Actividad, derivada de Activida Base
-public class ActividadEjercicio extends ActividadBase {
-
-    public ActividadEjercicio(String tipo , String nombre, int duracion, boolean completada) {
-        super(tipo, nombre, duracion, completada);
-    }
-
-    
-}
--- ActividadEstudio.java
+++
@@ -1,9 +0,0 @@
-//Clase dedicada a la Actividad, derivada de Activida Base
-public class ActividadEstudio extends ActividadBase {
-
-    public ActividadEstudio(String tipo, String nombre, int duracion, boolean completada) {
-        super(tipo, nombre, duracion, completada);
-    }
-
-   
-}
